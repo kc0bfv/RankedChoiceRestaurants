92413a27f8141d7c01eccfbe7b83167b716e7a19
kc0bfv <kc0bfv@gmail.com>
2016-12-11 01:18:31 -0500

    passenger deployment fix

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,8 +2,6 @@ import sys
 import os
 import os.path
 
-from django.core.wsgi import get_wsgi_application
-
 # These next two lines are only required if your default python isn't python3
 INTERP = os.path.expanduser("~/opt/bin/python3.5")
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
@@ -13,4 +11,5 @@ sys.path.append(os.path.join(os.getcwd(), "RankedChoiceRestaurants"))
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "RankedChoiceRestaurants.settings"
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
